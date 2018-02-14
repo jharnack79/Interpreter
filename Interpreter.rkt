d012b57b1877d4b65c8ce9de4eb57add05d948da
Jason Harnack <jfh79@case.edu>
2018-02-14 18:14:02 -0500

    Adding exterior files and initial interpreter file

---
+++ Interpreter.rkt
@@ -0,0 +1,12 @@
+; Jason Harnack, Tom Pescatore, Vicki Kelly
+; EECS 345 Interpreter Project
+
+; 1. Write a function called interpret
+; 2. Funciton thatll store the states
+; 3. Basic state evaluator
+; 4. Returning a value
+; 5. Create a parse tree
+;   - if, variable declaration, assignment, return, while
+; 6. Arithmetic evaluator 
+
+(load "simpleParser.scm")
