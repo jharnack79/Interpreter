3396df86490fbdbb397b9a62da7ae9a900a91b2b
Jason Harnack <jfh79@case.edu>
2018-03-07 21:00:19 -0500

    Adding throw and fixing issues with all state CPS methods

--- Interpreter.rkt
+++ Interpreter.rkt
@@ -23,40 +23,61 @@
 ;Main function to take filename and begin evaluating
 (define Interpret
   (lambda (fileName)
-    (Evaluate (parser fileName) emptyState))) 
+    (Evaluate (parser fileName) emptyState (lambda (v) v)))) 
 
-;Evaluates the parsed text file 
+;Evaluates the parsed text file
 (define Evaluate 
-  (lambda (lis state)
+  (lambda (lis state return)
     (cond
-      ((null? lis) state)
-      ((pair? (getRemaining lis)) (Evaluate (getRemaining lis) (SelectState (getFirst lis) state)))
-      (else (SelectState (getFirst lis) state)))))
+      ((null? lis) (return state))
+      ((pair? (getRemaining lis))  (SelectState (getFirst lis) state (lambda (v) (Evaluate (cdr lis) v return))))
+      (else (SelectState (getFirst lis) state return)))))
        
 ;Handles all state expressions and evaluates them based on given operation
 (define SelectState
-  (lambda (stmt state)
+  (lambda (stmt state return)
     (cond
-      ((eq? (getFirst stmt) 'var) (Mvar stmt state))
-      ((eq? (getFirst stmt) '=) (Massign stmt state))
-      ((eq? (getFirst stmt) 'if) (Mif stmt state))
-      ((eq? (getFirst stmt) 'while) (Mwhile stmt state))
-      ((eq? (getFirst stmt) 'return) (Mreturn stmt state))
+      ((eq? (getFirst stmt) 'var) (Mvar-cps stmt state return))
+      ((eq? (getFirst stmt) '=) (Massign-cps stmt state return))
+      ((eq? (getFirst stmt) 'if) (Mif-cps stmt state return))
+      ((eq? (getFirst stmt) 'while) (Mwhile-cps stmt state return))
+      ((eq? (getFirst stmt) 'return) (Mreturn-cps stmt state return))
+      ((eq? (getFirst stmt) 'begin) (Mbegin-cps stmt (cons emptyState state) return))
+      ((eq? (getFirst stmt) 'break) (Mbreak state return))
+      ((eq? (getFirst stmt) 'throw) (return (cadr stmt)))
       (else (error "Unknown function")))))
-      
+
+(define Mbegin-cps
+  (lambda (stmt state return)
+    (cond
+      ((null? lis) (return (cdr state)))
+      ((pair? (getRemaining lis)) (SelectState (getFirst lis) state (lambda (v) (Mbegin-cps (cdr lis) v return))))
+      (else (SelectState (getFirst lis) state return)))))
+
+(define Mbreak
+  (lambda (state return)
+    (cond
+      ((null? (cdr state)) (error "Cannot Break on Base Level"))
+      (else (return (cdr state))))))
+
 ;Variable Declaration operation
 ;Throws an error if the value has been declared, otherwise it will step through the state until it reaches the end
 ;will then append new variable with a value of () to the current state
-(define Mvar-cps
+;Need to abstract all the cars and cdrs
+(define Mvar-cps ; THIS IS QUESTIONABLE RIGHT NOW - IF NOT WORKING REMOVE (CAR STATE) ON EVERYTHING
   (lambda (stmt state return)
     (cond
-      ((member (getSecond stmt) (getVars state)) (return (error "Variable already declared")))
-      ((pair? (cddr stmt)) (return (Massign (list '= (getSecond stmt) (getThird stmt)) (cons (append (getVars state) (list (cadr stmt))) (list (append (getValues state) '(()) ))))))
-      (else (return (cons (append (getVars state) (list (cadr stmt))) (list (append (getValues state) '(()) ))))))))
+      ((isItAlreadyDeclared (getSecond stmt) (getVars state)) (return (error "Variable already declared")))
+      ((pair? (cddr stmt)) (Massign-cps (list '= (getSecond stmt) (getThird stmt)) (cons (append (getVars (car state)) (list (cadr stmt))) (list (append (getValues (car state)) '(()) ))) return))
+      (else (return (cons (append (getVars (car state)) (list (cadr stmt))) (list (append (getValues (car state)) '(()) ))))))))
 
-(define Mvar
-  (lambda (stmt state)
-    (Mvar-cps stmt state (lambda (v) v))))
+(define isItAlreadyDeclared
+  (lambda (varName state)
+    (cond
+      ((null? (car state)) #f) 
+      ((atom? (caar state)) (list? (member varName (car state))))
+      ((list? (member varName (caar state))) #t)
+      (else (isItAlreadyDeclared varName (cdr state))))))
 
 ;Stmt format (= variableName (expression or number) )
 ;Assignment operation for declared variables
@@ -64,19 +85,30 @@
   (lambda (stmt state return)
     (cond
       ((not (member (cadr stmt) (getVars state))) (return (error "Variable Not declared")))
-      (else (return (UpdateValue (getSecond stmt) (M_value (getThird stmt) state) state))))))
+      (else (M_value (getThird stmt) state (lambda (v) (UpdateValueState (getSecond stmt) v return)))))))
+ 
 
-(define Massign
-  (lambda (stmt state)
-    (Massign-cps stmt state (lambda (v) v))))
+;Helper update function to iterate through each state layer in order to find the layer in which the variable exists
+(define UpdateValueState
+  (lambda (varName newValue state return)
+    (cond
+      ((or (null? (car state)) (atom? (caar state))) (UpdateValue varName newValue state return))
+      ((not (member varName (caar state))) (cons (car state) (UpdateValueState varName newValue (cdr state) return)))
+      (else (UpdateValue varName newValue (car state) (lambda (v) (return (cons v (cdr state)))))))))
+
+(define atom?
+  (lambda (x)
+    (and (not (pair? x)) (not (null? x)))))
 
 ;Given a declared variable and a new value, the state is updated 
 (define UpdateValue
-  (lambda (varName newValue state)
+  (lambda (varName newValue state return)
     (cond
-      ((eq? (getFirstVar state) varName) (list (getVars state) (cons newValue (cdadr state))))
-      (else (list (cons (getFirstVar state) (car (UpdateValue varName newValue (getStateWithoutFirstPair state))))
-                  (cons (getFirstValue state) (cadr (UpdateValue varName newValue (getStateWithoutFirstPair state)))))))))
+      ((eq? (getFirstVar state) varName) (return (list (getVars state) (cons newValue (cdadr state)))))
+      (else (UpdateValue varName newValue (getStateWithoutFirstPair state)
+                         (lambda (v1) (UpdateValue varName newValue (getStateWithoutFirstPair state)
+                                                   (lamdba (v2) (return (list (cons (getFirstVar state) v1) (cons (getFirstValue state) v2)))))))))))
+   
 
 ;Handles all potential arithmetic and boolean expression evaluations
 (define M_value-cps
@@ -122,6 +154,13 @@
   (lambda (e)
     (caddr e)))
 
+;Helper function to iterate through each state until it finds the state that has the declared variable and its value
+(define GetVarValueState
+  (lambda (varName state)
+    (cond
+      ((or (null? (car state)) (atom? (caar state))) (GetVarValue varName state))
+      ((not (member varName (caar state))) (GetVarValueState varName (cdr state)))
+      (else (GetVarValue varName (car state))))))
         
 ;Recurses by loping off the first element of the vars and vals list
 (define GetVarValue
@@ -130,7 +169,6 @@
       ((not (member varName (getVars state))) (error "Variable Not Declared"))
       ((eq? varName (caar state)) (if (null? (caadr state)) (error "variable not assigned") (caadr state)))
       (else (GetVarValue varName (getStateWithoutFirstPair state))))))
-
 
 ;Returns the correct value when broken down to either a number, boolean, or variable
 (define Mreturn
@@ -146,24 +184,16 @@
 
 ;Takes the if statement and assess the expression after it,
 ;Depending on the result, it will execute either the then or else statement associated
-(define Mif
-  (lambda (stmt state)
-    (Mif-cps stmt state (lambda (v) v))))
-
 (define Mif-cps
   (lambda (stmt state return)
     (cond
-      ((M_value (getSecond stmt) state) (return (Evaluate (list (getThird stmt)) state)))
-      ((pair? (cdddr stmt)) (return (Evaluate (list (cadddr stmt)) state)))
+      ((M_value (getSecond stmt) state) (Evaluate (list (getThird stmt)) state return))
+      ((pair? (cdddr stmt)) (Evaluate (list (cadddr stmt)) state return))
       (else (return state)))))
 
 ;Takes while loop and evaluates given loop body if the while condition is true
-(define Mwhile
-  (lambda (stmt state)
-    (Mwhile-cps stmt state (lambda (v) v))))
-
 (define Mwhile-cps
   (lambda (stmt state return)
     (cond
-      ((M_value (getSecond stmt) state) (return (Mwhile-cps stmt (Evaluate (list (getThird stmt)) state) (lambda (v) v))))
+      ((M_value (getSecond stmt) state) (Evaluate (list (getThird stmt)) state (lambda (v) (Mwhile-cps stmt v return))))
       (else (return state)))))
